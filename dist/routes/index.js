5d7b1c6518842947e1f568d267c7ba204760c241
javierpb275 <javierpb275@gmail.com>
2021-12-29 21:39:30 +0100

    getting all task in route get /tasks. also formatted routes/index.ts file

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,20 +1,34 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const Task_1 = __importDefault(require("../models/Task"));
 const router = (0, express_1.Router)();
-router.get("/tasks", (req, res) => {
-    res.send("get /tasks");
-});
-router.get("/tasks/:id", (req, res) => {
-    res.send("get /tasks/:id");
-});
-router.post("/tasks", (req, res) => {
-    res.send("post /tasks");
-});
-router.put("/tasks/:id", (req, res) => {
-    res.send("put /tasks/:id");
-});
-router.delete("/tasks/:id", (req, res) => {
-    res.send("delete /tasks/:id");
-});
+router.get("/tasks", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const tasks = yield Task_1.default.find();
+    return res.send(tasks);
+}));
+router.get("/tasks/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    return res.send("get /tasks/:id");
+}));
+router.post("/tasks", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    return res.send("post /tasks");
+}));
+router.put("/tasks/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    return res.send("put /tasks/:id");
+}));
+router.delete("/tasks/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    return res.send("delete /tasks/:id");
+}));
 exports.default = router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,25 +1,39 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import Task, { ITask } from "../models/Task";
 
 const router: Router = Router();
 
-router.get("/tasks", (req, res) => {
-  res.send("get /tasks");
+router.get("/tasks", async (req: Request, res: Response): Promise<Response> => {
+  const tasks: ITask[] = await Task.find();
+  return res.send(tasks);
 });
 
-router.get("/tasks/:id", (req, res) => {
-  res.send("get /tasks/:id");
-});
+router.get(
+  "/tasks/:id",
+  async (req: Request, res: Response): Promise<Response> => {
+    return res.send("get /tasks/:id");
+  }
+);
 
-router.post("/tasks", (req, res) => {
-  res.send("post /tasks");
-});
+router.post(
+  "/tasks",
+  async (req: Request, res: Response): Promise<Response> => {
+    return res.send("post /tasks");
+  }
+);
 
-router.put("/tasks/:id", (req, res) => {
-  res.send("put /tasks/:id");
-});
+router.put(
+  "/tasks/:id",
+  async (req: Request, res: Response): Promise<Response> => {
+    return res.send("put /tasks/:id");
+  }
+);
 
-router.delete("/tasks/:id", (req, res) => {
-  res.send("delete /tasks/:id");
-});
+router.delete(
+  "/tasks/:id",
+  async (req: Request, res: Response): Promise<Response> => {
+    return res.send("delete /tasks/:id");
+  }
+);
 
 export default router;
